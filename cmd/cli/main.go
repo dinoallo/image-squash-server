7c74273d9a2a30152dcd0319f6ecfb0203e57bba
lingdie <lingdie.yy@outlook.com>
2025-05-15 11:06:44 +0800

    feat: add cli for squash

---
+++ cmd/cli/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"github.com/containerd/nerdctl/pkg/clientutil"
+	"github.com/lingdie/image-squash-server/pkg/options"
+	"github.com/lingdie/image-squash-server/pkg/runtime"
+)
+
+var containerdAddress string
+var namespace string
+var sourceImageRef string
+var targetImageRef string
+var squashLayerCount int
+
+func main() {
+	flag.StringVar(&containerdAddress, "containerd-address", "unix:///var/run/containerd/containerd.sock", "containerd address")
+	flag.StringVar(&namespace, "namespace", "k8s.io", "containerd namespace")
+	flag.StringVar(&sourceImageRef, "source-image", "", "source image reference")
+	flag.StringVar(&targetImageRef, "target-image", "", "target image reference")
+	flag.IntVar(&squashLayerCount, "squash-layer-count", 2, "squash layer count")
+	flag.Parse()
+	containerdClient, _, _, err := clientutil.NewClient(
+		context.Background(),
+		namespace,
+		containerdAddress,
+	)
+	if err != nil {
+		log.Fatalf("Failed to create containerd client: %v", err)
+	}
+
+	runtime, err := runtime.NewRuntime(
+		containerdClient,
+		namespace,
+	)
+	if err != nil {
+		log.Fatalf("Failed to create runtime: %v", err)
+	}
+
+	err = runtime.Squash(context.Background(), options.Option{
+		SourceImage:      sourceImageRef,
+		TargetImage:      targetImageRef,
+		SquashLayerCount: squashLayerCount,
+	})
+	if err != nil {
+		log.Fatalf("Failed to squash image: %v", err)
+	}
+
+}
