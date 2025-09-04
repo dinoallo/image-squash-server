e7f9c9af6c8f3a26e2fb83de4ed91b9d9def9885
Yun Pan <dinoallo@netc.it>
2025-09-04 15:06:44 +0800

    refactor: remove unused SquashImage struct from images package
    Signed-off-by: Yun Pan <dinoallo@netc.it>

--- pkg/images/images.go
+++ pkg/images/images.go
@@ -6,13 +6,6 @@ import (
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
-type SquashImage struct {
-	ClientImage containerd.Image
-	Config      ocispec.Image
-	Image       images.Image
-	Manifest    *ocispec.Manifest
-}
-
 type Image struct {
 	ClientImage containerd.Image
 	Config      ocispec.Image
