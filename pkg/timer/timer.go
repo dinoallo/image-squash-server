a263b48c753a7d240954f0bd1ecf7ed9a9dc601e
Yun Pan <dinoallo@netc.it>
2025-09-18 23:37:03 +0800

    feat: implement timer package with Timer interface and TimerImpl struct
    Signed-off-by: Yun Pan <dinoallo@netc.it>

---
+++ pkg/timer/timer.go
@@ -0,0 +1,30 @@
+package timer
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Timer interface {
+	Track(start time.Time, funcName string)
+}
+
+type TimerImpl struct {
+	*logrus.Logger
+}
+
+func NewTimerImpl(logger *logrus.Logger) (*TimerImpl, error) {
+	if logger == nil {
+		return nil, fmt.Errorf("logger is nil")
+	}
+	return &TimerImpl{
+		Logger: logger,
+	}, nil
+}
+
+func (t *TimerImpl) Track(start time.Time, funcName string) {
+	elapsed := time.Since(start)
+	t.Infof("(%s) cost %s", elapsed.Truncate(time.Millisecond).String(), funcName)
+}
